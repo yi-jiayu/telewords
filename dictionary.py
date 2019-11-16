209c0394a3a759a3974b5d3ceb57974a6f32568f
Jiayu Yi <yijiayu@gmail.com>
2019-11-17 00:08:09 +0800

    Use NLTK and WordNet for definitions

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,7 @@
-import sqlite3
-
-conn = sqlite3.connect("dictionary.sqlite")
+from nltk.corpus import wordnet as wn
 
 
 def get_definition(word):
-    c = conn.cursor()
-    c.execute("select definition from words where word = ?", (word,))
-    row = c.fetchone()
-    if row is not None:
-        return summarise(row[0])
-
-
-def summarise(definition):
-    return "\n\n".join(
-        line if len(line) <= 100 else line[:100] + "..."
-        for line in definition.split("\n\n")
-    )
+    synsets = wn.synsets(word)
+    if synsets:
+        return "\n".join(f"({s.pos()}) {s.definition()}" for s in synsets)
