39cdd037cde3ac264cadf94fbef35382e52a757d
Jiayu Yi <yijiayu@gmail.com>
2021-01-07 11:41:58 +0800

    Initial commit

---
+++ game.py
@@ -0,0 +1,28 @@
+import random
+
+from nltk.corpus import wordnet as wn
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def new_game():
+    attempts = 0
+    while True:
+        attempts += 1
+        letters = random.sample(alphabet, 7)
+        words = [w for w in wn.all_lemma_names() if
+                 4 <= len(w) <= 7 and set(w).issubset(set(letters)) and letters[3] in w]
+        for word in words:
+            if set(word) == set(letters):
+                print('Pangram:', word)
+                print('Generation attempts:', attempts)
+                print('Number of words:', len(words))
+                return letters, words
+
+
+if __name__ == '__main__':
+    letters, words = new_game()
+    print(letters)
+    for w in words:
+        lemmas = wn.lemmas(w)
+        print(w, ':', lemmas[0].synset().definition())
